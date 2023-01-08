16f973a6c7e1359bbb69bcf0b9251515754c005a
Adrian Furmanek <adrian.furm@gmail.com>
2023-01-08 23:50:59 +0100

    searchbox

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const filterRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFB</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filterRobots}/>
+            </div>
+        );
+    }
+}
+
+export default App;

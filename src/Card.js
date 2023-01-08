eb61a624444a9e4957e60c3f11b70da789ae65e4
Adrian Furmanek <adrian.furm@gmail.com>
2023-01-08 22:55:28 +0100

    card and card list components

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='face' src={`https://robohash.org/${id}?200x200`}></img>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;

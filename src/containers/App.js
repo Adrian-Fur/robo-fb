61a6b4eb007948527c2af0829bbb45951da69633
Adrian Furmanek <adrian.furm@gmail.com>
2023-01-10 00:38:42 +0100

    scroll component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 import "./App.css";
+import Scroll from "../components/Scroll";
 
 class App extends React.Component {
   constructor() {
@@ -35,7 +36,9 @@ class App extends React.Component {
         <div className="tc">
           <h1 className="f1">RoboFB</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filterRobots} />
+          <Scroll>
+            <CardList robots={filterRobots} />
+          </Scroll>
         </div>
       );
     }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'tachyons'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./containers/App";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
 
